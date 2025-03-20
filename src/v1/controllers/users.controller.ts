8e2c8de067fb51c968dea7986a5592870f764fc4
kungbi <dobby12335@gmail.com>
2025-03-20 14:25:43 +0900

    feat: getUser 관련 스키마 생성 및 적용

--- src/v1/controllers/users.controller.ts
+++ src/v1/controllers/users.controller.ts
@@ -1,5 +1,12 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { getUserParamsSchema } from '../schemas/user.schema.js';
 
-export async function findUserController(request: FastifyRequest, reply: FastifyReply) {
+export async function findUserController(request: FastifyRequest<{
+  Params: z.infer<typeof getUserParamsSchema>
+}>, reply: FastifyReply) {
+  const { id } = request.params;
+
+  request.log.info(`user id: ${id}`);
   reply.code(200).send('hello');
 }
---
+++ src/v1/schemas/user.schema.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+
+export const getUserParamsSchema = z.object({
+    id: z.string().min(1)
+});
