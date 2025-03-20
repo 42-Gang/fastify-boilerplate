c81a8f750709b1839bb5928d98cc2fa16f8059ac
kungbi <dobby12335@gmail.com>
2025-03-20 14:43:34 +0900

    feat: 커스텀 에러 정의 및 적용

--- src/v1/controllers/users.controller.ts
+++ src/v1/controllers/users.controller.ts
@@ -1,12 +1,17 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 import { getUserParamsSchema } from '../schemas/user.schema.js';
+import { ForbiddenException } from '../exceptions/core.error.js';
 
 export async function findUserController(request: FastifyRequest<{
   Params: z.infer<typeof getUserParamsSchema>
 }>, reply: FastifyReply) {
-  const { id } = request.params;
+  const id = parseInt(request.params.id);
+
 
   request.log.info(`user id: ${id}`);
+  if (id === 1) {
+    throw new ForbiddenException("You can't access this user");
+  }
   reply.code(200).send('hello');
 }
