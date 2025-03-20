e6b2b46e64cf1c766d5d5d5e112c65e774434c29
kungbi <kipper12335@gmail.com>
2025-03-20 11:18:06 +0900

    refactor: request, response schema validation 적용

--- src/v1/routes/auth.route.ts
+++ src/v1/routes/auth.route.ts
@@ -1,11 +1,39 @@
 import { FastifyInstance } from 'fastify';
 
 import { loginController, signupController } from '../controllers/auth.controller.js';
+import {
+  loginRequestSchema,
+  loginResponseSchema,
+  signupRequestSchema,
+  signupResponseSchema,
+} from '../schemas/auth.schema.js';
 
 export default async function authRoutes(fastify: FastifyInstance) {
   // Signup route
-  fastify.post('/', signupController);
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: signupRequestSchema,
+        response: {
+          201: signupResponseSchema,
+        },
+      },
+    },
+    signupController,
+  );
 
   // Login route
-  fastify.post('/login', loginController);
+  fastify.post(
+    '/login',
+    {
+      schema: {
+        body: loginRequestSchema,
+        response: {
+          201: loginResponseSchema,
+        },
+      },
+    },
+    loginController,
+  );
 }
