6d1d850f2a87bfdada4d91113f4cff3b84a14f61
kungbi <kipper12335@gmail.com>
2025-03-22 18:34:50 +0900

    fix: jwt, prisma mocking 추가

--- test/v1/services/auth.service.spec.ts
+++ test/v1/services/auth.service.spec.ts
@@ -3,17 +3,37 @@ import { signupService, loginService } from '../../../src/v1/services/auth.servi
 import { z } from 'zod';
 import { loginRequestSchema, signupRequestSchema } from '../../../src/v1/schemas/auth.schema';
 import { STATUS } from '../../../src/v1/constants/status';
+import { describe, expect, it, vi } from 'vitest';
+import { JWT } from '@fastify/jwt';
+
+vi.mock('../../../src/v1/utils/prisma.ts', async (importOriginal) => {
+  return {
+    default: {
+      user: {
+        create: vi.fn(),
+        findUnique: vi.fn(),
+      },
+    },
+  };
+});
+import prisma from '../../../src/v1/utils/prisma';
 
 describe('Auth Service', () => {
   const mockLogger: FastifyBaseLogger = {
-    info: jest.fn(),
-    error: jest.fn(),
-    warn: jest.fn(),
-    debug: jest.fn(),
-    trace: jest.fn(),
-    fatal: jest.fn(),
-    child: jest.fn(),
+    info: vi.fn(),
+    error: vi.fn(),
+    warn: vi.fn(),
+    debug: vi.fn(),
+    trace: vi.fn(),
+    fatal: vi.fn(),
+    child: vi.fn(),
   } as unknown as FastifyBaseLogger;
+
+  const mockJwt: JWT = {
+    sign: vi.fn(),
+    verify: vi.fn(),
+    decode: vi.fn(),
+  } as unknown as JWT;
 
   describe('signupService', () => {
     it('should return success status and message', async () => {
@@ -22,6 +42,14 @@ describe('Auth Service', () => {
         password: 'testpassword',
         name: 'testname',
       };
+
+      (prisma.user.create as any).mockResolvedValue({
+        id: 1,
+        email: 'testuser',
+        password_hash: 'testpassword',
+        name: 'testname',
+        two_factor_enabled: false,
+      });
 
       const response = await signupService(data, mockLogger);
 
@@ -39,7 +67,15 @@ describe('Auth Service', () => {
         password: 'testpassword',
       };
 
-      const response = await loginService(data, mockLogger);
+      (prisma.user.findUnique as any).mockResolvedValue({
+        id: 1,
+        email: 'testuser',
+        password_hash: 'testpassword',
+        name: 'testname',
+        two_factor_enabled: false,
+      });
+      mockJwt.sign.mockReturnValue('tmp-access-token');
+      const response = await loginService(data, mockLogger, mockJwt);
 
       expect(response).toEqual({
         status: STATUS.SUCCESS,
