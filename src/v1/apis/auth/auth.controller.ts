30a0c0b80fb9e9655b6850cd00f91938bc09046b
kungbi <kipper12335@gmail.com>
2025-03-23 20:13:20 +0900

    refactor: simplify import statements in auth.controller.ts

--- src/v1/apis/auth/auth.controller.ts
+++ src/v1/apis/auth/auth.controller.ts
@@ -1,10 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 
-import {
-  signupService,
-  loginService,
-  generateRefreshToken,
-} from '../../apis/auth/auth.service.js';
+import { signupService, loginService, generateRefreshToken } from '../../apis/auth/auth.service.js';
 import { loginRequestSchema, signupRequestSchema } from '../../common/schemas/auth.schema.js';
 
 export async function signupController(request: FastifyRequest, reply: FastifyReply) {
