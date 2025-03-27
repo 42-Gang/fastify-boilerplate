903c2e8c4a75470a50b1f2eaaf575bfde6afaaa3
kungbi <kipper12335@gmail.com>
2025-03-27 11:57:54 +0900

    feat: DI 컨테이너 설정 분리 및 적용
    컨테이너 설정 관련 코드를 container.ts로 분리하고 main.ts에 적용.

---
+++ src/container.ts
@@ -0,0 +1,35 @@
+import { FastifyInstance } from 'fastify';
+import { diContainer, fastifyAwilixPlugin } from '@fastify/awilix';
+import { asClass, asValue, Lifetime } from 'awilix';
+import prisma from './v1/common/utils/prisma.js';
+
+export async function setDiContainer(server: FastifyInstance) {
+  server.register(fastifyAwilixPlugin, {
+    disposeOnClose: true,
+    disposeOnResponse: true,
+    strictBooleanEnforced: true,
+  });
+  diContainer.register({
+    prisma: asValue(prisma),
+    jwt: asValue(server.jwt),
+    logger: asValue(server.log),
+  });
+  await diContainer.loadModules(['./**/*.repository.js'], {
+    esModules: true,
+    formatName: 'camelCase',
+    resolverOptions: {
+      lifetime: Lifetime.SINGLETON,
+      register: asClass,
+      injectionMode: 'CLASSIC',
+    },
+  });
+  await diContainer.loadModules(['./**/*.controller.js', './**/*.service.js'], {
+    esModules: true,
+    formatName: 'camelCase',
+    resolverOptions: {
+      lifetime: Lifetime.SINGLETON,
+      register: asClass,
+      injectionMode: 'CLASSIC',
+    },
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import Fastify, { FastifyInstance } from 'fastify';
+import Fastify from 'fastify';
 import closeWithGrace from 'close-with-grace';
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 
 import app from './app.js';
 import swaggerPlugin from './v1/common/utils/swagger-plugin.js';
-import { diContainer, fastifyAwilixPlugin } from '@fastify/awilix';
-import { asClass, asValue, Lifetime } from 'awilix';
-import prisma from './v1/common/utils/prisma.js';
 import jwtPlugin from './v1/common/plugins/jwt-plugin.js';
+import { setDiContainer } from './container.js';
 
 function getLoggerOptions() {
   if (process.stdout.isTTY) {
@@ -44,37 +42,6 @@ async function startServer(server: Fastify.FastifyInstance) {
     server.log.error(err);
     process.exit(1);
   }
-}
-
-async function setDiContainer(server: FastifyInstance) {
-  server.register(fastifyAwilixPlugin, {
-    disposeOnClose: true,
-    disposeOnResponse: true,
-    strictBooleanEnforced: true,
-  });
-  diContainer.register({
-    prisma: asValue(prisma),
-    jwt: asValue(server.jwt),
-    logger: asValue(server.log),
-  });
-  await diContainer.loadModules(['./**/*.repository.js'], {
-    esModules: true,
-    formatName: 'camelCase',
-    resolverOptions: {
-      lifetime: Lifetime.SINGLETON,
-      register: asClass,
-      injectionMode: 'CLASSIC',
-    },
-  });
-  await diContainer.loadModules(['./**/*.controller.js', './**/*.service.js'], {
-    esModules: true,
-    formatName: 'camelCase',
-    resolverOptions: {
-      lifetime: Lifetime.SINGLETON,
-      register: asClass,
-      injectionMode: 'CLASSIC',
-    },
-  });
 }
 
 async function init() {
