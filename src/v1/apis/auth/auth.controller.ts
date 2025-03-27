c9307362b9502a4c62291a3b9b00a9e9450605bf
kungbi <kipper12335@gmail.com>
2025-03-27 15:25:38 +0900

    refactor(auth): try-catch 에러 핸들링 제거
    app.ts 에서 중앙 에러 핸들러를 두었기 때문에 try catch가 따로 필요하지 않습니다..!

--- src/v1/apis/auth/auth.controller.ts
+++ src/v1/apis/auth/auth.controller.ts
@@ -7,31 +7,21 @@ export default class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   signup = async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const body = signupRequestSchema.parse(request.body);
-      request.log.info(body, 'Signup request received');
-      const result = await this.authService.signup(body);
-      reply.status(201).send(result);
-    } catch (error) {
-      request.log.error(error, 'Error in signup');
-      reply.status(400).send({ error });
-    }
+    const body = signupRequestSchema.parse(request.body);
+    request.log.info(body, 'Signup request received');
+    const result = await this.authService.signup(body);
+    reply.status(201).send(result);
   };
 
   login = async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-      const body = loginRequestSchema.parse(request.body);
-      const result = await this.authService.login(body);
-      const refreshToken = await this.authService.generateRefreshToken();
+    const body = loginRequestSchema.parse(request.body);
+    const result = await this.authService.login(body);
+    const refreshToken = await this.authService.generateRefreshToken();
 
-      reply.header(
-        'set-cookie',
-        `refreshToken=${refreshToken}; HttpOnly; SameSite=Strict; Secure; Path=/; Max-Age=3600;`,
-      );
-      reply.status(200).send(result);
-    } catch (error) {
-      request.log.error(error, 'Error in login');
-      reply.status(401).send({ error });
-    }
+    reply.header(
+      'set-cookie',
+      `refreshToken=${refreshToken}; HttpOnly; SameSite=Strict; Secure; Path=/; Max-Age=3600;`,
+    );
+    reply.status(200).send(result);
   };
 }
