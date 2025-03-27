8ba3233edc0c2b2620735a4c626a89701e1e8c8e
kungbi <kipper12335@gmail.com>
2025-03-27 22:27:15 +0900

    feat(users): Redis 캐싱 적용

--- src/v1/apis/users/users.service.ts
+++ src/v1/apis/users/users.service.ts
@@ -3,19 +3,37 @@ import { z } from 'zod';
 import { FindUserResponseSchema } from './users.schema.js';
 import { STATUS } from '../../common/constants/status.js';
 import UserRepositoryInterface from '../../repositories/persistent/interfaces/user.repository.interface.js';
+import { UserCacheInterface } from '../../repositories/volatile/interfaces/user.cache.interface.js';
+import { FastifyBaseLogger } from 'fastify';
 
 export default class UsersService {
-  constructor(private readonly userRepository: UserRepositoryInterface) {}
+  constructor(
+    private readonly userRepository: UserRepositoryInterface,
+    private readonly userCacheRepository: UserCacheInterface,
+    private readonly logger: FastifyBaseLogger,
+  ) {}
 
   async findUser(id: number): Promise<z.infer<typeof FindUserResponseSchema>> {
+    const cachedUser = await this.userCacheRepository.getUserById(id);
+    if (cachedUser) {
+      this.logger.info('User found in cache');
+      return {
+        status: STATUS.SUCCESS,
+        data: {
+          id: cachedUser.id,
+          name: cachedUser.name,
+          email: cachedUser.email,
+          createdAt: cachedUser.created_at,
+          updatedAt: cachedUser.updated_at,
+        },
+      };
+    }
     const user = await this.userRepository.findById(id);
+
     if (!user) {
       throw new NotFoundException('User not found');
     }
-
-    // this.logger.info(this.userCacheRepository, 'UserCacheRepository');
-    // await this.userCacheRepository.setUserById(id, user);
-    // this.logger.info(await this.userCacheRepository.getUserById(id), 'User found in cache');
+    await this.userCacheRepository.setUserById(id, user);
 
     return {
       status: STATUS.SUCCESS,
