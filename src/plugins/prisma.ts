db6db189c10d70b4cc152bd853930a794646f358
kungbi <kipper12335@gmail.com>
2025-03-29 22:53:03 +0900

    refactor: prisma 설정 변경 및 dotenv 추가

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,7 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+import * as process from 'node:process';
+
+dotenv.config();
+
+const env = process.env;
+const dbUrl = `${env.DB_SYSTEM}://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:${env.DB_PORT}/${env.DB_NAME}`;
 
 const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
+  datasources: {
+    db: {
+      url: dbUrl,
+    },
+  },
+  errorFormat: 'pretty',
 });
 
 export default prisma;
