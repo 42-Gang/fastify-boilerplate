29d785ffa6893c21aa6180b755949385171f30cd
Jeongsu <latsyrc900@gmail.com>
2025-03-29 12:39:08 +0900

    feat: :art: got 라이브러리 사용한 http client 추가

--- src/v1/common/http/got/http.client.ts
+++ src/v1/common/http/got/http.client.ts
@@ -1,25 +1,20 @@
 import got from 'got';
 import { HttpClient, HttpRequestOptions } from '../interface/http.client.interface.js';
 
-export class GotHttpClient implements HttpClient {
-  constructor(private baseURL: string) {} // 서버별 다른 주소를 가지나?
-
+export const gotClient: HttpClient = {
   async request(options: HttpRequestOptions): Promise<any> {
-    const { method, url, body, queryParams, headers } = options;
-
     const client = got.extend({
-      prefixUrl: this.baseURL,
       responseType: 'json',
       timeout: { request: 10000 },
     });
 
-    const response = await client(url, {
-      method,
-      json: body,
-      searchParams: queryParams,
-      headers,
+    const response = await client(options.url, {
+      method: options.method,
+      json: options.body,
+      searchParams: options.queryParams,
+      headers: options.headers,
     });
 
     return response.body;
   }
-}
+};
