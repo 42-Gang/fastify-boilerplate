99a2350a2383f017aaf6303fc29e92584e6bbac2
kungbi <kipper12335@gmail.com>
2025-03-29 14:33:03 +0900

    refactor: 소켓 서버 설정 삭제

--- src/socket.ts
+++ src/socket.ts
@@ -2,32 +2,8 @@ import { Server } from 'socket.io';
 import { FastifyInstance } from 'fastify';
 import { registerSocketGateway } from './v1/sockets/gateway.js';
 
-interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
-}
-
-interface ClientToServerEvents {
-  hello: () => void;
-}
-
-interface InterServerEvents {
-  ping: () => void;
-}
-
-interface SocketData {
-  name: string;
-  age: number;
-}
-
 export function createSocketServer(fastify: FastifyInstance) {
-  const socket = new Server<
-    ClientToServerEvents,
-    ServerToClientEvents,
-    InterServerEvents,
-    SocketData
-  >(fastify.server, {
+  const socket = new Server(fastify.server, {
     cors: {
       origin: '*',
     },
